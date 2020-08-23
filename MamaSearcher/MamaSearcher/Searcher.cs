ada216499e89310729c7941e8152b2bb025793b7
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-23 16:33:12 +0300

    Implemented the PerformSearchFunction

---
+++ MamaSearcher/MamaSearcher/Searcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MamaSearcher
+{
+    public class Searcher : IMamaSearcher
+    {
+        public Dictionary<string, Action<int, string>> PatternsToAction { get; set; }
+        public List<string> Patterns { get; set; }
+        public void PerformSearch(string content)
+        {
+            foreach (KeyValuePair<string, Action<int, string>> pair in PatternsToAction)
+            {
+                if(content.Contains(pair.Key))
+                {
+                    int index = Patterns.IndexOf(pair.Key);
+                    pair.Value.Invoke(index, pair.Key);
+                }
+            }
+        }
+
+        public void Subscribe(string pattern, Action<int, string> actionToPerform)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
